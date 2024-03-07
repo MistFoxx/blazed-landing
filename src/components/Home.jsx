fb32a511f55a9d3f973199f66d49a7b1e9f689d2
MistFoxx <charlie.szafron@itas.ca>
2024-03-06 18:14:52 -0800

    fix?

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,13 +9,13 @@ export default function Home() {
         <p className="text-2xl mt-4">LAUNCHING 2024</p>
         <p className="text-xl mt-4 mx-5 lg:mx-24">Follow us on <a href="https://www.instagram.com/getblazed.ca/" className="hover:text-blazedgreen">Instagram</a> to keep up with updates and be the first to know when we go live!</p>
       </div>
-      <div className="flex justify-center items-center m-auto w-full lg:w-3/5 max-h-screen">
+      <div className="flex justify-center items-center m-auto w-full lg:w-1/2 max-h-screen">
         <Image
         src="/images/david-gabric-KdC5agsz6ik-unsplash.jpg"
         alt="Silhouette of a person holding up a cannabis leaf in front of the sun low in the sky. Photo by David Gabric on Unsplash."
         width={380}
         height={400}
-        className="rounded-tl-full rounded-tr-full mb-5 h-96 w-80 lg:w-1/2 lg:h-1/2 object-cover"
+        className="rounded-tl-full rounded-tr-full mb-5 h-96 w-80 lg:w-3/5 lg:h-1/2 object-cover"
         />
       </div>
     </div>
